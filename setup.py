663df82f5eb8569c502c8a0e44a586b0a8e023f2
Giorgos Filandrianos <geofila@outlook.com.gr>
2023-07-24 15:51:12 +0300

    Fix Demo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("app/README.md", "r") as f:
 
 setup(
     name="cece",
-    version="0.0.09",
+    version="0.0.03",
     description="Conceptual Edits as Counterfactual Explanations",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
